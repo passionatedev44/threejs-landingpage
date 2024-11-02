4d1e071f9a862a9c04254fc3a7364886c595ac32
passionatedev44 <181296915+passionatedev44@users.noreply.github.com>
2024-11-02 20:12:23 +0530

    Initial commit

---
+++ src/Ground.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { MeshReflectorMaterial } from "@react-three/drei";
+import { RepeatWrapping, TextureLoader, LinearSRGBColorSpace } from "three";
+
+export function Ground() {
+  // thanks to https://polyhaven.com/a/rough_plasterbrick_05 !
+  const [roughness, normal] = useLoader(TextureLoader, [
+    process.env.PUBLIC_URL + "textures/terrain-roughness.jpg",
+    process.env.PUBLIC_URL + "textures/terrain-normal.jpg",
+  ]);
+
+  useEffect(() => {
+    [normal, roughness].forEach((t) => {
+      t.wrapS = RepeatWrapping;
+      t.wrapT = RepeatWrapping;
+      t.repeat.set(5, 5);
+      t.offset.set(0, 0);
+    });
+
+    // Set texture color space
+    normal.colorSpace = LinearSRGBColorSpace;
+  }, [normal, roughness]);
+
+  useFrame((state, delta) => {
+    let t = -state.clock.getElapsedTime() * 0.128;
+    roughness.offset.set(0, t % 1);
+    normal.offset.set(0, t % 1);
+  });
+
+  return (
+    <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+      <planeGeometry args={[30, 30]} />
+      <MeshReflectorMaterial
+        envMapIntensity={0}
+        normalMap={normal}
+        normalScale={[0.15, 0.15]}
+        roughnessMap={roughness}
+        dithering={true}
+        color={[0.015, 0.015, 0.015]}
+        roughness={0.7}
+        blur={[1000, 400]} // Blur ground reflections (width, height), 0 skips blur
+        mixBlur={30} // How much blur mixes with surface roughness (default = 1)
+        mixStrength={80} // Strength of the reflections
+        mixContrast={1} // Contrast of the reflections
+        resolution={1024} // Off-buffer resolution, lower=faster, higher=better quality, slower
+        mirror={0} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+        depthScale={0.01} // Scale the depth factor (0 = no depth, default = 0)
+        minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+        maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+        depthToBlurRatioBias={0.25} // Bias for depth-to-blur ratio (default = 0.25)
+        debug={0}
+        reflectorOffset={0.2} // Offset for the virtual camera (default = 0)
+      />
+    </mesh>
+  );
+}
